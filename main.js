4dc2f0bbfba2cfe5ca58af4be9e28898f5d980cf
Alef Lobo <38471735+aleflobo-dev@users.noreply.github.com>
2021-10-23 18:49:55 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const LinksSocialMedia = {
+  github: 'aleflobo-dev',
+  youtube: 'channel/UCHzhURJ4Z28wchh3K6iQ4Kw',
+  facebook: 'alef.lobo.562',
+  instagram: 'aleflobo_',
+  twitter: 'aleflobo_'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+
+    alert(li.children[0].href)
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      UserImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+getGitHubProfileInfos()
